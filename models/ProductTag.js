26e26b189868cabfd603e5eecbb33970540da0ab
Bort <93967399+strudelAndCoffee@users.noreply.github.com>
2022-03-23 23:19:18 -0500

    set up cross table associations

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -20,6 +20,14 @@ ProductTag.init(
         model: 'product',
         key: 'id'
       }
+    },
+    tag_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: 'tag',
+        key: 'id'
+      }
     }
   },
   {
