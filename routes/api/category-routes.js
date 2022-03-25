5bbc61df49c5da627300ea0d42fd8f35b4f8bd65
Bort <93967399+strudelAndCoffee@users.noreply.github.com>
2022-03-25 01:04:20 -0500

    delete cat rout finished

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -81,8 +81,24 @@ router.put('/:id', (req, res) => {
   });
 });
 
+// delete a category by its `id` value
 router.delete('/:id', (req, res) => {
-  // delete a category by its `id` value
+  Category.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+  .then(dbCatData => {
+    if (!dbCatData) {
+      res.status(404).json({ message: "No category found with this ID. "});
+      return;
+    }
+    res.json(dbCatData);
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 module.exports = router;
